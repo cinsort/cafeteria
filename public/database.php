5128964b75bdd0472441037cef5fcd155b53501d
cinsort <milkofhuk@gmail.com>
2022-03-30 20:57:48 +0700

    fifth

--- public/database.php
+++ public/database.php
@@ -1,10 +1,12 @@
 <?php
 $dbConn = pg_connect('host=postgres dbname=app user=postgres password=password')
-or die('Cannot connect to DB: ' . pg_last_error());
+    or die('Cannot connect to DB: ' . pg_last_error());
 $dbInfo = pg_version($dbConn);
-//foreach ($dbInfo as $key) echo $key.'  ';
+
 pg_query($dbConn,
     "CREATE TABLE IF NOT EXISTS 
-        users(id INTEGER PRIMARY KEY, name VARCHAR(25), password VARCHAR(100))"
+    users(
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(25),
+    password VARCHAR(100))"
 ) or die("Cannot create users table");
-//echo 'created';
